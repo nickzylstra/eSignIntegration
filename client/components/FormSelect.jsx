8083fa288a6e1415716aae823a5ea1572bc16025
nickzylstra <nick.zylstra@gmail.com>
2020-01-13 14:11:24 -0800

    Setup forms and signers pickers

--- client/components/FormSelect.jsx
+++ client/components/FormSelect.jsx
@@ -3,30 +3,41 @@ import React, { useState } from 'react';
 const FormSelect = (props) => {
   const { forms, signers } = props;
 
+  const defaultFormVal = '--- please select form ---';
+  const defaultSignerVal = '--- please select signer ---';
+  const [formVal, setFormVal] = useState(defaultFormVal);
+  const [signersVal, setSignersVal] = useState(defaultSignerVal);
 
   return (
     <form>
       <select
-        name="forms"
-        value="Choose form"
+        name="form"
+        value={formVal}
+        onChange={({ target }) => setFormVal(target.value)}
       >
+        <option value={defaultFormVal} disabled>{defaultFormVal}</option>
         {forms.map(({ name, templateId }) => (
-          <option key={templateId} id={templateId}>
-            {name}
-          </option>
+          <option key={templateId} value={templateId}>{name}</option>
         ))}
       </select>
+
       <br />
+
       <select
-        name="signers"
+        name="signer"
+        value={signersVal}
+        onChange={({ target }) => setSignersVal(target.value)}
       >
+        <option value={defaultSignerVal} disabled>{defaultSignerVal}</option>
         {signers.map(({
           name, contactId, emails, organization,
         }) => (
-          <option key={contactId} id={contactId}>
+          <option key={contactId} value={contactId}>
             {name}
-            {emails[0]}
+            {' of '}
             {organization}
+            {' email: '}
+            {emails[0]}
           </option>
         ))}
       </select>
