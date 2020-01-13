883f025d730612fcb0e56542a9439a71b65ee6ad
nickzylstra <nick.zylstra@gmail.com>
2020-01-13 11:03:28 -0800

    Move folderId selection back into server

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 // TODO - get org id from Welkin request to start app
-// using docusign folder Id for now
-const org = { orgId: '0df7af7f-945d-4b38-a58d-6225f94c8b07' };
+const org = { orgId: 'TODO' };
 ReactDOM.render(<App org={org} />, document.getElementById('app'));
--- server/controllers/docusign/listTemplates.js
+++ server/controllers/docusign/listTemplates.js
@@ -10,6 +10,7 @@ module.exports = async function listTemplates(orgId) {
   dsApi.addDefaultHeader('Authorization', `Bearer ${dsJwtAuth.accessToken}`);
   dsApi.setBasePath(dsJwtAuth.basePath);
   const templatesApi = new docusign.TemplatesApi(dsApi);
-  const options = { folderIds: orgId };
+  // TODO - refactor to retrieve folderId for orgId
+  const options = { folderIds: '0df7af7f-945d-4b38-a58d-6225f94c8b07' };
   return templatesApi.listTemplates(dsJwtAuth.accountId, options);
 };
