aaf5e1c377b3e6915e17a6c689ec6c8e657b6117
nickzylstra <nick.zylstra@gmail.com>
2020-01-12 19:27:06 -0800

    Stub out server parts to req forms from DS

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-// TODO - get org id from Welkin call to start app
+// TODO - get org id from Welkin request to start app
 const org = { id: '1234325' };
 ReactDOM.render(<App org={org} />, document.getElementById('app'));
--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const compression = require('compression');
+const fancy = require('fancy-log');
 
 
 const app = express();
@@ -9,5 +10,16 @@ app.use(compression());
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
+app.get('forms', async (req, res) => {
+  const { org } = req.query;
+  try {
+    // request org forms from docusign
+    // request signers from database or Welkin
+    // respond with list and signers
+  } catch (error) {
+    fancy(error);
+    res.status(500).send('server error getting forms');
+  }
+});
 
 module.exports = app;
