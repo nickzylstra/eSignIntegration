6a1d710bf14495ba3941100a0fc858b8ec9486cd
nickzylstra <nick.zylstra@gmail.com>
2020-01-12 19:14:23 -0800

    Stub out client start up function

--- client/App.jsx
+++ client/App.jsx
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor(props) {
     super(props);
+    const { org } = props;
     this.state = {
-
+      org,
+      templates: [],
+      signers: [],
     };
+  }
+
+  componentDidMount() {
+    const { org } = this.state;
+    // retrieve templates and signers from server
+    // update state
   }
 
   render() {
--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+// TODO - get org id from Welkin call to start app
+const org = { id: '1234325' };
+ReactDOM.render(<App org={org} />, document.getElementById('app'));
