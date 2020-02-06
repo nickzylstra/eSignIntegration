f6950980013b5b534359385fa6e89c8f8393df58
nickzylstra <nick.zylstra@gmail.com>
2020-02-05 16:18:43 -0800

    Update app test for rendering fetched data

--- client/App.test.jsx
+++ client/App.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axiosMock from 'axios';
+import { fireEvent, waitForElement } from '@testing-library/react';
 import customRender from '../test-utils/customRender.jsx';
 import App from './App.jsx';
 
@@ -8,12 +9,14 @@ jest.mock('axios');
 const host = 'http://localhost:3000';
 
 describe('App', () => {
-  test('loads app component and fetches data', () => {
+  const form1Name = 'form1';
+  const signer1Name = 'signer1';
+  test('loads app component and fetches data', async () => {
     axiosMock
       .mockResolvedValueOnce({
         data: {
           envelopeTemplates: [{
-            name: 'form1',
+            name: form1Name,
             templateId: 'id1',
           }],
         },
@@ -21,7 +24,7 @@ describe('App', () => {
       .mockResolvedValueOnce({
         data: {
           contacts: [{
-            name: 'signer1',
+            name: signer1Name,
             contactId: 'id1',
             organization: 'org1',
             emails: ['email1'],
@@ -29,8 +32,12 @@ describe('App', () => {
         },
       });
 
-    const { getByLabelText } = customRender(<App org="test" host={host} />);
+    const { getByLabelText, getByText } = customRender(<App org="test" host={host} />);
     const label = getByLabelText(/app/i);
     expect(label).toBeInTheDocument();
+    const templateName = await waitForElement(() => getByText(form1Name));
+    expect(templateName).toBeInTheDocument();
+    const signerName = await waitForElement(() => getByText(new RegExp(`${signer1Name}`)));
+    expect(signerName).toBeInTheDocument();
   });
 });
