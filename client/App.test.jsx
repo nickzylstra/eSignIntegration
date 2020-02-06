d0c80554b53d2c14e51d19077ecfc4d61a091500
nickzylstra <nick.zylstra@gmail.com>
2020-02-05 16:00:15 -0800

    Update app tests to mock axios on loading calls

--- client/App.test.jsx
+++ client/App.test.jsx
@@ -5,10 +5,31 @@ import App from './App.jsx';
 
 jest.mock('axios');
 
+const host = 'http://localhost:3000';
 
 describe('App', () => {
-  test('renders app component', () => {
-    const { getByLabelText } = customRender(<App org="test" />);
+  test('loads app component and fetches data', () => {
+    axiosMock
+      .mockResolvedValueOnce({
+        data: {
+          envelopeTemplates: [{
+            name: 'form1',
+            templateId: 'id1',
+          }],
+        },
+      })
+      .mockResolvedValueOnce({
+        data: {
+          contacts: [{
+            name: 'signer1',
+            contactId: 'id1',
+            organization: 'org1',
+            emails: ['email1'],
+          }],
+        },
+      });
+
+    const { getByLabelText } = customRender(<App org="test" host={host} />);
     const label = getByLabelText(/app/i);
     expect(label).toBeInTheDocument();
   });
