95a5d0c10f4199f85a8b32794bf246fe4d5ba387
nickzylstra <nick.zylstra@gmail.com>
2020-02-06 12:36:49 -0800

    Stub db schema

---
+++ database/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const fancy = require('fancy-log');
+
+
+module.exports = (async () => {
+  const host = process.env.MONGO_HOST || 'mongodb://localhost/';
+  const dbName = 'esignIntegration';
+
+  try {
+    const db = await mongoose.connect(`${host}${dbName}`, { useNewUrlParser: true });
+    fancy(`mongoose connection error to host: "${host}" for db: "${dbName}"`);
+
+    // session schema / model
+    //   token
+    //     id
+    //     expires
+    //   welkin
+    //     providerId
+    //     patientId
+    //     workedId
+
+    // provider schema / model
+    //   providerId
+    //   docusign
+    //     clientId
+    //     impersonatedUserGuid
+    //     privateKey
+
+    return {
+      db,
+    };
+  } catch (error) {
+    fancy(`mongoose connected to host: "${host}" for db: "${dbName}"`);
+    return {};
+  }
+})();
